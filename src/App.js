3ad36b641664e7733d11bffe29014b9d6a19b044
gouravjeet <gouravjeet@gmail.com>
2018-10-10 23:31:46 -0400

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
             My Blog
           </p>
         </header>
+        <Home />
       </div>
     );
   }
